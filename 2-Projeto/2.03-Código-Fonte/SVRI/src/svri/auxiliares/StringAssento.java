eaaca74f69dbd34720f2e6d8db2b67f8b87086d2
Daniel de Oliveira Vasconcelos <daniel.oliveiravas@gmail.com>
2015-05-19 14:31:13 -0300

    Adicionando classe StringAssento, transforma String em um arrayList de Assento e vice-versa.
    Rastreabilidade: Caso de Uso Comprar Ingresso, Manter Cadastro de Sala

---
+++ 2-Projeto/2.03-Código-Fonte/SVRI/src/svri/auxiliares/StringAssento.java
@@ -0,0 +1,37 @@
+package svri.auxiliares;
+
+import java.util.ArrayList;
+
+import svri.entidades.Assento;
+
+public class StringAssento {
+
+	/*Função utilizada para converter uma string do banco de dados (Entidades sala e sessao)
+	 * em um arrayList de Assento. */
+	public static ArrayList<Assento> converterStringParaAssento(String string){
+		ArrayList<Assento> assentos = new ArrayList<>();
+		
+		String[] paresDePosicoes = string.split(";");
+		for(int i=0;i<paresDePosicoes.length;i++){
+			String[] numeros = paresDePosicoes[i].split(",");
+			Assento assento = new Assento();
+			assento.setFileira(Integer.parseInt(numeros[0]));
+			assento.setColuna(Integer.parseInt(numeros[1]));
+			assentos.add(assento);
+		}
+		return  assentos;
+	}
+	
+	public static String converterAssentoParaString(ArrayList<Assento> assentos){
+		String string = "";
+		
+		for (Assento assento : assentos) {
+			string += String.valueOf(assento.getFileira());
+			string += ",";
+			string += String.valueOf(assento.getColuna());
+			string += ";";
+		}
+		
+		return string;
+	}
+}
