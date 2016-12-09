33ecba3bd79b4791225212bd4c72231e88719063
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2016-12-09 00:38:54 -0200

    #55 Adiciona anotação @FuncionalInterface

--- src/main/java/br/ufg/inf/pitanga/servicos/InterfacePagamento.java
+++ src/main/java/br/ufg/inf/pitanga/servicos/InterfacePagamento.java
@@ -3,6 +3,7 @@ package br.ufg.inf.pitanga.servicos;
 import br.ufg.inf.pitanga.entidades.Cliente;
 import br.ufg.inf.pitanga.entidades.Compra;
 
+@FunctionalInterface
 public interface InterfacePagamento {
     void realizaPagamento(Compra novaCompra, Cliente cliente);
 }
