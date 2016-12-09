31b3b22b32983b5ccbf1fa3685f2dbc8980d88b6
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2016-12-09 19:58:23 -0200

    #71 Remove import não utilizado

--- src/main/java/br/ufg/inf/pitanga/servicos/GeraPDFServico.java
+++ src/main/java/br/ufg/inf/pitanga/servicos/GeraPDFServico.java
@@ -6,7 +6,6 @@ import com.lowagie.text.Paragraph;
 import com.lowagie.text.pdf.PdfWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
