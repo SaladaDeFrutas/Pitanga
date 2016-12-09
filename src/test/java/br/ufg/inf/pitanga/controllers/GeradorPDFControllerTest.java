e595624965b3436ad625df63fb1d0da3495e4231
Gabriela Aimée <gabrielaimeeg@hotmail.com>
2016-12-09 18:01:53 -0200

    #71 Criada classe de testes para GeradorPDFController.

---
+++ src/test/java/br/ufg/inf/pitanga/controllers/GeradorPDFControllerTest.java
@@ -0,0 +1,17 @@
+package br.ufg.inf.pitanga.controllers;
+
+import org.junit.Before;
+
+public class GeradorPDFControllerTest {
+
+    GeradorPDFController geradorPDFController;
+
+    @Before
+    public void criaNovaInstanciaDeGeradorPDFController(){
+        geradorPDFController = new GeradorPDFController();
+    }
+
+
+
+
+}
